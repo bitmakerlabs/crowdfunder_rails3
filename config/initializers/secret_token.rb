4b9c24be3d88bbe3b8a73205eda6d693608fc4ee
Khurram Virani <kvirani@gmail.com>
2013-02-09 18:15:03 -0500

    Initial Commit
    Brand new rails app, but no public/index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdfunder::Application.config.secret_token = '027d3c8a9406c5904637104518162f79bd31ace1a84812a122c1ef75e40694d51be4d0569257a955df28736125c7c44f020638ef480ecf090360c475ff3d0435'
